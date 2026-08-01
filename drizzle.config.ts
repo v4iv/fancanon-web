26428ad9a77cc726a20bea4e6062887cf61a9b2d
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-08-01 19:27:10 +0530

    try: using neondb serverless driver on prod

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'drizzle-kit'
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set')
+if (!process.env.DIRECT_URL) throw new Error('DIRECT_URL is not set')
 
 export default defineConfig({
   schema: './src/lib/server/db/schema.ts',
   dialect: 'postgresql',
-  dbCredentials: { url: process.env.DATABASE_URL },
+  dbCredentials: { url: process.env.DIRECT_URL },
   verbose: true,
   strict: true,
 })
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,11 +1,23 @@
-import postgres from 'postgres'
-import { drizzle } from 'drizzle-orm/postgres-js'
+import { dev } from '$app/env'
 import { DATABASE_URL } from '$app/env/private'
+import postgres from 'postgres'
+import { neon } from '@neondatabase/serverless'
+import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
+import { drizzle as drizzlePostgres } from 'drizzle-orm/postgres-js'
 
 import * as schema from './schema'
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set')
 
-const client = postgres(DATABASE_URL)
+let db: any
 
-export const db = drizzle(client, { schema })
+if (dev) {
+  const client = postgres(DATABASE_URL)
+
+  db = drizzlePostgres(client, { schema })
+} else {
+  const client = neon(DATABASE_URL)
+  db = drizzleNeon(client, { schema })
+}
+
+export { db }
