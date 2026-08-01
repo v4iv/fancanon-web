1cd54092a352a47bcfcb2fe9c3bcb56d3b7ef5cf
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-08-01 19:37:48 +0530

    feat: improve db initialization with dynamic imports

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,23 +1,26 @@
 import { dev } from '$app/env'
 import { DATABASE_URL } from '$app/env/private'
-import postgres from 'postgres'
-import { neon } from '@neondatabase/serverless'
-import { drizzle as drizzleNeon } from 'drizzle-orm/neon-http'
-import { drizzle as drizzlePostgres } from 'drizzle-orm/postgres-js'
 
 import * as schema from './schema'
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set')
 
-let db: any
+export const db = await (async () => {
+  if (dev) {
+    const [{ drizzle }, { default: postgres }] = await Promise.all([
+      import('drizzle-orm/postgres-js'),
 
-if (dev) {
-  const client = postgres(DATABASE_URL)
+      import('postgres'),
+    ])
 
-  db = drizzlePostgres(client, { schema })
-} else {
-  const client = neon(DATABASE_URL)
-  db = drizzleNeon(client, { schema })
-}
+    return drizzle(postgres(DATABASE_URL), { schema })
+  }
 
-export { db }
+  const [{ drizzle }, { neon }] = await Promise.all([
+    import('drizzle-orm/neon-http'),
+
+    import('@neondatabase/serverless'),
+  ])
+
+  return drizzle(neon(DATABASE_URL), { schema })
+})()
