0448edaa10c0ca6b3f968ee2b432a4b3b240e4fd
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2026-08-21 18:47:26 +0530

    fix: skip internal router on app crash

--- src/lib/components/navbar/navbar-mini.svelte
+++ src/lib/components/navbar/navbar-mini.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { ORIGIN } from '$app/env/public'
-
   import { m } from '$lib/paraglide/messages'
   import logo from '$lib/assets/logo.svg'
   import uwu from '$lib/assets/kawaii/uwu-shadow.svg'
@@ -10,13 +8,15 @@
 <header class="flex w-full items-center justify-center border-b p-4">
   {#if uwuState.enabled}
     <!-- uwu -->
-    <a href="/">
+    <!-- data-sveltekit-reload tells SvelteKit to skip its internal router and let the browser perform a native HTTP request for /. -->
+    <a href="/" data-sveltekit-reload>
       <img src={uwu} alt="fancanon logo that links to home" class="h-8 w-auto" />
       <span class="sr-only">{m['app-name']()}</span>
     </a>
   {:else}
     <!-- logo & app name -->
-    <a href={ORIGIN} class="flex items-center gap-2">
+    <!-- data-sveltekit-reload tells SvelteKit to skip its internal router and let the browser perform a native HTTP request for /. -->
+    <a href="/" class="flex items-center gap-2" data-sveltekit-reload>
       <img
         src={logo}
         alt="fancanon logo that links to home"
